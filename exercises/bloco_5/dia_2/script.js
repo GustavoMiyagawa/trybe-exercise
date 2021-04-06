53401baadce833be23f2d393e7a9ee06b853d228
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-04-06 14:34:44 -0400

    1. Adicionando a tag h1 com texto.

---
+++ exercises/bloco_5/dia_2/script.js
@@ -0,0 +1,4 @@
+let seletorBody = document.querySelector('body');
+let titleH1 = document.createElement('h1');
+titleH1.innerText = 'Exercício 5.2 - JavaScript Dom';
+seletorBody.appendChild(titleH1);
