3ae5e0ca9db20c07c66a8a47c84e398d6f8f3b86
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-04-06 14:44:09 -0400

    2. Adicionando a tag div com a classe.

--- exercises/bloco_5/dia_2/script.js
+++ exercises/bloco_5/dia_2/script.js
@@ -2,3 +2,7 @@ let seletorBody = document.querySelector('body');
 let titleH1 = document.createElement('h1');
 titleH1.innerText = 'Exercício 5.2 - JavaScript Dom';
 seletorBody.appendChild(titleH1);
+
+let divCreator = document.createElement('div');
+divCreator.className = '.main-content';
+seletorBody.appendChild(divCreator);
