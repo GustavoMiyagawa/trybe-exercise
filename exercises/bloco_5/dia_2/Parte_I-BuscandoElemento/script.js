9bbc04258ffa753f3b3a32c2de47e7ef1a22411e
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-04-18 16:48:19 -0400

    Exercício 4 e 5.

--- exercises/bloco_5/dia_2/Parte_I-BuscandoElemento/script.js
+++ exercises/bloco_5/dia_2/Parte_I-BuscandoElemento/script.js
@@ -7,3 +7,13 @@ pai.style.backgroundColor = 'red';
 
 //3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
 document.querySelector('#primeiroFilhoDoFilho').innerText = 'primeiroFilhoDoFilho';
+
+//4. Acesse o primeiroFilho a partir de pai.
+document.querySelector('#pai').firstChild;
+
+//5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling
+
+//6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+//7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+//8. Agora acesse o terceiroFilho a partir de pai .
