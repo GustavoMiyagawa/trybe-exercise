bd00ebbf60afba36426c0efa72fcbe55a6a0320a
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-04-18 21:34:28 -0400

    Criado uma função para alterar o texto do elemento com classe .tech.

--- exercises/bloco_5/dia_3/Conteúdo/main.js
+++ exercises/bloco_5/dia_3/Conteúdo/main.js
@@ -18,9 +18,14 @@ function select(e) {
 container.addEventListener('click', select);
 
 /*3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
+com a classe 'tech';*/
+function inputText() {
+  document.querySelector('.tech').innerText = input.value;
+}
 
-4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+input.addEventListener('keyup', inputText);
+
+/*4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
 4.1. Que tal redirecionar para seu portifólio?
 
